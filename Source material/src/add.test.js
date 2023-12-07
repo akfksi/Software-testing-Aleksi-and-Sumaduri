84d35344aa1ccfd7ed718941e76029bd1d4c31bd
Aleksi <aleksi.myllyniemi@tuni.fi>
2023-12-07 14:45:19 +0200

    Added test cases

--- Source material/src/add.test.js
+++ Source material/src/add.test.js
@@ -3,4 +3,5 @@ import add from './add.js'
 
 test ('adds two numbers correcly', () => {
     expect(add(1,2)).toBe(3);
+    expect(add(-2,2)).toBe(0);
 });
--- Source material/src/capitalize.test.js
+++ Source material/src/capitalize.test.js
@@ -2,5 +2,7 @@ import {expect, test} from '@jest/globals';
 import capitalize from './capitalize.js'
 
 test('Correctly capitalizes string', () => {
-    
+    expect(capitalize('MaNsaArI')).toBe('Mansaari');
+    expect(capitalize('MANSAARI')).toBe('Mansaari');
+    expect(capitalize('mansaari')).toBe('Mansaari');
 })
