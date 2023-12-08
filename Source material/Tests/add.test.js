c8b031e71d663382ab3a449c619a577dde5b340b
Aleksi <aleksi.myllyniemi@tuni.fi>
2023-12-08 18:02:00 +0200

    Typo fixed

--- Source material/Tests/add.test.js
+++ Source material/Tests/add.test.js
@@ -1,7 +1,7 @@
 import {expect, test} from '@jest/globals';
 import add from '../src/add.js'
 
-test ('adds two numbers correcly', () => {
+test ('adds two numbers correctly', () => {
     expect(add(1,2)).toBe(3);
     expect(add(-2,2)).toBe(0);
 });
